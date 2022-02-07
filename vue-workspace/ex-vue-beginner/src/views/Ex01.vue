adc22fd6a92efbe0cfea9b6166e4c30a92d60482
ryota-09 <n.ryo950911@gmail.com>
2022-02-07 14:29:12 +0900

    firstCommit

---
+++ vue-workspace/ex-vue-beginner/src/views/Ex01.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class XXXComponent extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
